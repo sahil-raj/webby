cd750c71b4d38d7b7791723018eb03f49034163b
sahil-raj <sahilrajaaru@gmail.com>
2024-01-26 15:39:01 +0530

    changes

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
+# from bs4 import BeautifulSoup
+# import requests
+# import json
+
+
+# class Webby:
+#     def __init__(self, u):
+#         self.url = u
+
+#     def getHtmlData(self):
+#         return requests.get(self.url)
+    
+#     def getTable(self, x):
+#         mySoup = BeautifulSoup(x.content, "html5lib")
+#         # for table in mySoup.find_all("table"):
+#         #     head = table.find_all('th')
+#         #     for tr in 
+#         for table in mySoup.find_all("table"):
+#             jsonD = json.dumps(table)
+#             print(jsonD)
+
+
+
+# if __name__ == "__main__":
+#     myObj = Webby("https://trends.builtwith.com/websitelist/Responsive-Tables")
+#     t = myObj.getHtmlData()
+#     myObj.getTable(t)
+
 from bs4 import BeautifulSoup
 import requests
 import json
 
-
 class Webby:
     def __init__(self, u):
         self.url = u
-
-    def getHtmlData(self):
-        d = requests.get(self.url)
-        return d.content
+        self.tables = []
     
-    def getTable(self, x):
-        mySoup = BeautifulSoup(x, "html5lib")
-        for table in mySoup.find_all("table"):
-            head = table.find_all('th')
-            print(head)
-
-
+    def getHtmlData(self):
+        return requests.get(self.url).text
+    
+    def getTablesFromHtml(self):
+        mySoup = BeautifulSoup(self.getHtmlData(), "html5lib")
+        # for t in mySoup.find_all("table"):
+        #     self.tables.append(t)
+        # print(mySoup.table.th)
+        for t in mySoup.table:
+            print(t)
 
 if __name__ == "__main__":
-    myObj = Webby("https://trends.builtwith.com/websitelist/Responsive-Tables")
-    t = myObj.getHtmlData()
-    print(myObj.getTables(t))
+    o = Webby("https://datatables.net/examples/basic_init/multiple_tables.html")
+    o.getTablesFromHtml()
