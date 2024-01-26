425192ec385d3082cabac7d5a30ac2e5bf918991
Janikaa17 <115789732+Janikaa17@users.noreply.github.com>
2024-01-26 15:40:33 +0530

    Update main.py
    still don't know how the pandas work but the table headings and the table data can be scrapped

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import pandas as pd
 
 
 class Webby:
-    def __init__(self, u):
-        self.url = u
+  def getTableData(self,url):
+        r = requests.get(url)
+        soup= BeautifulSoup(r.content,"html.parser")
+        tableHead = soup.thead
+        row_headers = []
+        for rows in tableHead.find_all('tr'):
+          for headers in rows.find_all('th'):
+            row_headers.append(headers.text)
+        print(row_headers)
+        tableBody= soup.tbody
+        table_body_values=[]
+        for rows in tableBody.find_all('tr')[1:]:
+          td_tags= rows.find_all('td')
+          td_values =[headers.text  for headers in td_tags]
+          table_body_values.append(td_values)
+        print(table_body_values)
+        df= pandas.DataFrame(table_body_values, columns=row_headers)
+        df.head()
 
-    def getHtmlData(self):
-        d = requests.get(self.url)
-        return d.content
-    
-    def getTable(self, x):
-        mySoup = BeautifulSoup(x, "html5lib")
-        for table in mySoup.find_all("table"):
-            head = table.find_all('th')
-            print(head)
 
+        
 
-
-if __name__ == "__main__":
-    myObj = Webby("https://trends.builtwith.com/websitelist/Responsive-Tables")
-    t = myObj.getHtmlData()
-    print(myObj.getTables(t))
+if __name__=="__main__":
+  print_table_data=Webby()
+  print_table_data.getTableData("https://trends.builtwith.com/websitelist/Responsive-Tables")
