d2dda30063d1f24793c89904668e5db4fa1f8c47
sahil-raj <sahilrajaaru@gmail.com>
2024-01-26 13:02:19 +0530

    some changes

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-#test comment
-#test comment 2
-print("test")
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+class Webby:
+    def __init__(self, u):
+        self.url = u
+
+    def getHtmlData(self):
+        d = requests.get(self.url)
+        return d.content
+    
+    def getTables(self, x):
+        mySoup = BeautifulSoup(x, "html5lib")
+        return mySoup.find_all("table")
+            
+
+
+
+if __name__ == "__main__":
+    myObj = Webby("https://trends.builtwith.com/websitelist/Responsive-Tables")
+    t = myObj.getHtmlData()
+    print(myObj.getTables(t))
